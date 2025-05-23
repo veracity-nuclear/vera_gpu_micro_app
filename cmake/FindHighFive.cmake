429502211c57b9006bcec889133e8fcf94449ae8
MicahBest <micahbest21@gmail.com>
2025-05-23 16:15:26 -0500

    Refine 2

--- cmake/FindHighFive.cmake
+++ cmake/FindHighFive.cmake
@@ -4,12 +4,22 @@
 # Locate HDF5 (required dependency)
 find_package(HDF5 REQUIRED COMPONENTS C HL)
 
+# Try to find the main HighFive header
+find_path(HIGHFIVE_INCLUDE_DIR
+  NAMES highfive/H5File.hpp
+  HINTS
+    ENV HIGHFIVE_ROOT
+    ${CMAKE_PREFIX_PATH}
+    /opt/highfive
+  PATH_SUFFIXES include
+)
+
 # Create an INTERFACE imported target for HighFive
 add_library(HighFive::HighFive INTERFACE IMPORTED)
 
 # Specify include directories
 target_include_directories(HighFive::HighFive INTERFACE
-  "/opt/highfive/include"
+  ${HIGHFIVE_INCLUDE_DIR}
   ${HDF5_INCLUDE_DIRS}
 )
 
