2568be61294008e7594e5fbc61d04aba121b6caa
MicahBest <micahbest21@gmail.com>
2025-05-23 15:22:38 -0500

    Take 4

--- cmake/FindHighFive.cmake
+++ cmake/FindHighFive.cmake
@@ -8,11 +8,5 @@ find_package(HDF5 REQUIRED COMPONENTS C HL)
 add_library(HighFive::HighFive INTERFACE IMPORTED)
 set_target_properties(HighFive::HighFive PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "/opt/highfive/include"
-)
-
-# Link HDF5 imported targets to HighFive interface
-# This ensures HDF5 symbols (e.g., H5Treclaim) are linked transitively
-target_link_libraries(HighFive::HighFive INTERFACE
-    HDF5::HDF5_C
-    HDF5::HDF5_HL
+    INTERFACE_LINK_LIBRARIES "${HDF5_LIBRARIES}"
 )
