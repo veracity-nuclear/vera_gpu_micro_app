99931331e09e208a044a89f2c1f3877f0af50605
MicahBest <micahbest21@gmail.com>
2025-05-23 15:29:45 -0500

    Take 6

--- cmake/FindHighFive.cmake
+++ cmake/FindHighFive.cmake
@@ -1,4 +1,4 @@
-# Fallback CMake module to locate HighFive and export a proper interface target
+# Fallback CMake module to locate HighFive and export an interface target
 # Hardcode include path to /opt/highfive/include
 
 # Locate HDF5 (required dependency)
@@ -6,8 +6,13 @@ find_package(HDF5 REQUIRED COMPONENTS C HL)
 
 # Create an INTERFACE imported target for HighFive
 add_library(HighFive::HighFive INTERFACE IMPORTED)
-set_target_properties(HighFive::HighFive PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "/opt/highfive/include"
-    INTERFACE_LINK_LIBRARIES "${HDF5_C_LIBRARIES};${HDF5_HL_LIBRARIES}"
+
+# Specify include directories
+target_include_directories(HighFive::HighFive INTERFACE
+  "/opt/highfive/include"
 )
 
+# Link HDF5 libraries transitively (using variables for broad compatibility)
+target_link_libraries(HighFive::HighFive INTERFACE
+  ${HDF5_LIBRARIES}
+)
