b6cb12cbc530d3a027e7cc33594bf529edf2c216
Aaron M. Graham <aaron.graham@veracitynuclear.com>
2025-05-27 11:15:14 -0400

    Add more serial_moc tests

--- moc/tests/test_serial_moc_1region_1angle.cpp
+++ moc/tests/test_serial_moc_1region_1angle.cpp
@@ -3,18 +3,6 @@
 #include <gtest/gtest.h>
 #include "../src/serial_moc.hpp"
 
-TEST(BasicTest, AssertTrue) {
-    EXPECT_TRUE(true);
-}
-
-TEST(BasicTest, AssertFalse) {
-    EXPECT_FALSE(false);
-}
-
-TEST(BasicTest, AssertEqual) {
-    EXPECT_EQ(1, 1);
-}
-
 TEST(BasicTest, cart_1region_7g_1a_1p_serial) {
     const std::vector<std::string> args = {"moc_tests", "data/cart_1region_7g_1a_1p_serial.h5", "data/c5g7.xsl"};
     double result = serial_moc_sweep(args);
---
+++ moc/tests/test_serial_moc_4region_1angle.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <gtest/gtest.h>
+#include "../src/serial_moc.hpp"
+
+TEST(BasicTest, cart_4region_7g_1a_1p_serial) {
+    const std::vector<std::string> args = {"moc_tests", "data/cart_4region_7g_1a_1p_serial.h5", "data/c5g7.xsl"};
+    double result = serial_moc_sweep(args);
+    EXPECT_NEAR(result, 0.73822736, 1.0e-8);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
