6ea02ab7244ad1025f3e361995530034c1ce5d62
Aaron Graham <aaron.graham@veracitynuclear.com>
2025-06-04 10:03:21 -0600

    Put a test parallel_for for every backend

--- moc/src/main.cpp
+++ moc/src/main.cpp
@@ -10,27 +10,48 @@ int main(int argc, char* argv[]) {
     std::cout << "Kokkos execution spaces enabled:\n";
 #ifdef KOKKOS_ENABLE_SERIAL
     std::cout << "  - SERIAL\n";
+    Kokkos::parallel_for("hello", Kokkos::RangePolicy<Kokkos::Serial>(0, 10), KOKKOS_LAMBDA(int i) {
+      printf("Hello from parallel_for test loop index i = %d/10\n", i);
+    });
 #endif
 #ifdef KOKKOS_ENABLE_OPENMP
     std::cout << "  - OPENMP\n";
+    Kokkos::parallel_for("hello", Kokkos::RangePolicy<Kokkos::OpenMP>(0, 10), KOKKOS_LAMBDA(int i) {
+      printf("Hello from parallel_for test loop index i = %d/10\n", i);
+    });
+#endif
+#ifdef KOKKOS_ENABLE_OPENMPTARGET
+    std::cout << "  - OPENMPTARGET\n";
+    Kokkos::parallel_for("hello", Kokkos::RangePolicy<Kokkos::OpenMPTarget>(0, 10), KOKKOS_LAMBDA(int i) {
+      printf("Hello from parallel_for test loop index i = %d/10\n", i);
+    });
 #endif
 #ifdef KOKKOS_ENABLE_THREADS
     std::cout << "  - THREADS\n";
+    Kokkos::parallel_for("hello", Kokkos::RangePolicy<Kokkos::Threads>(0, 10), KOKKOS_LAMBDA(int i) {
+      printf("Hello from parallel_for test loop index i = %d/10\n", i);
+    });
 #endif
 #ifdef KOKKOS_ENABLE_CUDA
     std::cout << "  - CUDA\n";
+    Kokkos::parallel_for("hello", Kokkos::RangePolicy<Kokkos::Cuda>(0, 10), KOKKOS_LAMBDA(int i) {
+      printf("Hello from parallel_for test loop index i = %d/10\n", i);
+    });
 #endif
 #ifdef KOKKOS_ENABLE_HIP
     std::cout << "  - HIP\n";
+    Kokkos::parallel_for("hello", Kokkos::RangePolicy<Kokkos::HIP>(0, 10), KOKKOS_LAMBDA(int i) {
+      printf("Hello from parallel_for test loop index i = %d/10\n", i);
+    });
 #endif
 #ifdef KOKKOS_ENABLE_SYCL
     std::cout << "  - SYCL\n";
+    Kokkos::parallel_for("hello", Kokkos::RangePolicy<Kokkos::SYCL>(0, 10), KOKKOS_LAMBDA(int i) {
+      printf("Hello from parallel_for test loop index i = %d/10\n", i);
+    });
 #endif
     std::cout << "Kokkos default backend: " << Kokkos::DefaultExecutionSpace::name() << "\n";
 
-    Kokkos::parallel_for("hello", 10, KOKKOS_LAMBDA(int i) {
-      printf("Hello from parallel_for test loop index i = %d/10\n", i);
-    });
   }
 
   const std::vector<std::string> args(argv, argv + argc);
