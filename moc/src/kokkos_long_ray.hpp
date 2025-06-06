cfabb9d7678a4dfa2bae8ae2d2b903dc62be90f1
Aaron M. Graham <aaron.graham@veracitynuclear.com>
2025-06-06 17:01:19 -0400

    Remove unneeded starting point on long rays

--- moc/src/kokkos_long_ray.hpp
+++ moc/src/kokkos_long_ray.hpp
@@ -9,7 +9,6 @@ struct KokkosLongRay
     public:
         std::vector<int> _fsrs;
         std::vector<double> _segments;
-        std::vector<std::pair<double, double>> _starting_points;
         std::vector<int> _bc_face;
         std::vector<int> _bc_index;
         double _radians;
@@ -26,12 +25,6 @@ struct KokkosLongRay
           _bc_face(group.getDataSet("BC_face").read<std::vector<int>>()),
           _bc_index(group.getDataSet("BC_index").read<std::vector<int>>())
           {
-            // Read starting points, converting to pairs (since we don't care about the Z coordinate)
-            auto points_data = group.getDataSet("Starting_Point").read<std::vector<double>>();
-            _starting_points.reserve(points_data.size() / 3);
-            for (int i = 0; i < points_data.size(); i += 3) {
-                _starting_points.push_back(std::make_pair(points_data[i], points_data[i + 1]));
-            };
             _bc_face[RAY_START] -= 1;
             _bc_face[RAY_END] -= 1;
             _bc_index[RAY_START] -= 1;
