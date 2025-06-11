e317c0d111bb973d4fb04d9a3f39cd3a5e0e934d
Aaron M. Graham <aaron.graham@veracitynuclear.com>
2025-06-11 14:20:46 -0400

    Add some kokkos sweeper tests

---
+++ moc/tests/test_kokkos_moc_7x7_omp4.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <gtest/gtest.h>
+#include "base_moc.hpp"
+#include "serial_moc.hpp"
+#include "eigen_solver.hpp"
+#include "argument_parser.hpp"
+
+TEST(BasicTest, 7x7_7g_16a_3p_serial) {
+    const char* raw_args[] = {"exe", "data/7x7_7g_16a_3p_serial.h5", "data/c5g7.xsl", "--sweeper", "kokkos", "--device", "--openmp", "--kokkos-num-threads=4"};
+    char** args = const_cast<char**>(raw_args);
+    auto parser = ArgumentParser::vera_gpu_moc_parser(raw_args[0]);
+    parser.parse(3, args);
+    BaseMOC* sweeper = new SerialMOC(parser);
+    EigenSolver solver(parser.get_args("test_serial_moc_7x7.exe"), sweeper);
+    solver.solve();
+    EXPECT_NEAR(solver.keff(), 1.34087940, 1.0e-7);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ moc/tests/test_kokkos_moc_pin_serial.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <gtest/gtest.h>
+#include "base_moc.hpp"
+#include "serial_moc.hpp"
+#include "eigen_solver.hpp"
+#include "argument_parser.hpp"
+
+TEST(BasicTest, pin_7g_16a_3p_serial) {
+    const char* raw_args[] = {"exe", "data/pin_7g_16a_3p_serial.h5", "data/c5g7.xsl", "--sweeper", "kokkos", "--device", "--serial"};
+    char** args = const_cast<char**>(raw_args);
+    auto parser = ArgumentParser::vera_gpu_moc_parser(raw_args[0]);
+    parser.parse(3, args);
+    BaseMOC* sweeper = new SerialMOC(parser);
+    EigenSolver solver(parser.get_args("test_serial_moc_pin.exe"), sweeper);
+    solver.solve();
+    EXPECT_NEAR(solver.keff(), 1.32569524, 1.0e-7);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
