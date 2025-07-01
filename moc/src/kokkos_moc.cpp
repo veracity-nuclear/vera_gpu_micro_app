594e9eb63b7e4f3cf172ce7fd5a48eac8a7ba96d
Aaron M. Graham <aaron.graham@veracitynuclear.com>
2025-07-01 12:49:09 -0400

    clean up some templating stuff

--- moc/src/kokkos_moc.cpp
+++ moc/src/kokkos_moc.cpp
@@ -168,11 +168,15 @@ KokkosMOC<ExecutionSpace>::KokkosMOC(const ArgumentParser& args) :
         }
     }
 
-    // Build exponential table
-#ifdef KOKKOS_ENABLE_CUDA
-    if constexpr (std::is_same_v<ExecutionSpace, Kokkos::Cuda>)
+// Build exponential table (for all execution spaces that want to use table lookup)
+    bool build_table = false;
+#ifdef KOKKOS_ENABLE_SERIAL
+    build_table = std::is_same_v<ExecutionSpace, Kokkos::Serial>;
 #endif
-    {
+#ifdef KOKKOS_ENABLE_OPENMP
+    build_table = std::is_same_v<ExecutionSpace, Kokkos::OpenMP> || build_table;
+#endif
+    if (build_table) {
         _n_exp_intervals = 40000;
         double min_val = -40.0;
         double max_val = 0.0;
@@ -417,39 +421,54 @@ void compute_exparg(int iray, int ig, int ipol,
                     const Kokkos::View<const double*, ExecutionSpace>& rsinpolang,
                     const Kokkos::View<const int*, ExecutionSpace>& ray_nsegs)
 {
-#ifdef KOKKOS_ENABLE_CUDA
-    if constexpr (!std::is_same_v<ExecutionSpace, Kokkos::Cuda>)
-#endif
-    {
-        for (int iseg = ray_nsegs(iray); iseg < ray_nsegs(iray + 1); iseg++) {
-            int local_seg = iseg - ray_nsegs(iray);
-            double val = -xstr(ray_fsrs(iseg), ig) * ray_segments(iseg) * rsinpolang(ipol);
-            int i = Kokkos::floor(val * rdx) + n_intervals + 1;
-            if (i >= 0 && i < n_intervals + 1) {
-                exparg(local_seg) = exp_table(i, 0) * val + exp_table(i, 1);
-            } else if (val < -700.0) {
-                exparg(local_seg) = 1.0;
-            } else {
-                exparg(local_seg) = 1.0 - Kokkos::exp(val);
-            }
+    for (int iseg = ray_nsegs(iray); iseg < ray_nsegs(iray + 1); iseg++) {
+        int local_seg = iseg - ray_nsegs(iray);
+        double val = -xstr(ray_fsrs(iseg), ig) * ray_segments(iseg) * rsinpolang(ipol);
+        int i = Kokkos::floor(val * rdx) + n_intervals + 1;
+        if (i >= 0 && i < n_intervals + 1) {
+            exparg(local_seg) = exp_table(i, 0) * val + exp_table(i, 1);
+        } else if (val < -700.0) {
+            exparg(local_seg) = 1.0;
+        } else {
+            exparg(local_seg) = 1.0 - Kokkos::exp(val);
         }
     }
 }
 
+#ifdef KOKKOS_ENABLE_CUDA
+template <>
+KOKKOS_INLINE_FUNCTION
+void compute_exparg<Kokkos::Cuda>(int iray, int ig, int ipol,
+                    const Kokkos::View<const double**, Kokkos::Cuda>& exp_table,
+                    int n_intervals, double rdx,
+                    Kokkos::View<double*, typename Kokkos::Cuda::scratch_memory_space>& exparg,
+                    const Kokkos::View<const double**, Kokkos::Cuda>& xstr,
+                    const Kokkos::View<const int*, Kokkos::Cuda>& ray_fsrs,
+                    const Kokkos::View<const double*, Kokkos::Cuda>& ray_segments,
+                    const Kokkos::View<const double*, Kokkos::Cuda>& rsinpolang,
+                    const Kokkos::View<const int*, Kokkos::Cuda>& ray_nsegs)
+{
+    return;
+}
+#endif
+
 template <typename ExecutionSpace>
 KOKKOS_INLINE_FUNCTION
-double eval_exp_arg(Kokkos::View<double*, typename ExecutionSpace::scratch_memory_space>& exparg,
+double eval_exp_arg(const Kokkos::View<double*, typename ExecutionSpace::scratch_memory_space>& exparg,
                     const int iseg, const double xstr, const double ray_segment, const double rsinpolang)
 {
-#ifdef KOKKOS_ENABLE_CUDA
-    if constexpr (std::is_same_v<ExecutionSpace, Kokkos::Cuda>) {
-        return 1.0 - Kokkos::exp(-xstr * ray_segment * rsinpolang);
-    } else
-#endif
-    {
-        return exparg(iseg);
-    }
+    return exparg(iseg);
 }
+
+#ifdef KOKKOS_ENABLE_CUDA
+template <>
+KOKKOS_INLINE_FUNCTION
+double eval_exp_arg<Kokkos::Cuda>(const Kokkos::View<double*, typename Kokkos::Cuda::scratch_memory_space>& exparg,
+                    const int iseg, const double xstr, const double ray_segment, const double rsinpolang)
+{
+    return 1.0 - Kokkos::exp(-xstr * ray_segment * rsinpolang);
+}
+#endif
 
 // Unified implementation of sweep
 template <typename ExecutionSpace>
