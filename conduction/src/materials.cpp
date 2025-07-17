4713f15664025aea3af5970a3b5e137759fe2126
MicahBest <micahbest21@gmail.com>
2025-07-17 09:48:09 -0400

    Updated materials docs and placement in file

--- conduction/src/materials.cpp
+++ conduction/src/materials.cpp
@@ -4,107 +4,19 @@
 Correlations for temperature-dependent properties are taken from the CTF v4.4 Manual
 https://info.ornl.gov/sites/publications/Files/Pub203334.pdf
 
-Materials are defined in alphabetical order.
-    - Helium
-    - UO2
+Materials:
+  Solid:
     - Zircaloy
+    - Fuel:
+      - UO2
+  Fluid:
+    - Argon
+    - Helium
+    - Hydrogen
+    - Krypton
+    - Nitrogen
+    - Xenon
 */
-
-double Helium::k(double T) const {
-    const double a = 1.314e-3; // in BTU/hr-ft-F
-    const double b = 0.668;
-    double T_R = T * 9.0 / 5.0; // Convert K to °R
-    double k = a * std::pow(T_R, b); // Conductivity in BTU/hr-ft-F
-    return k * 1.730735;   // Convert to W/m-K
-}
-
-double Xenon::k(double T) const {
-    const double a = 1.395e-5; // in BTU/hr-ft-F
-    const double b = 0.872;
-    double T_R = T * 9.0 / 5.0; // Convert K to °R
-    double k = a * std::pow(T_R, b); // Conductivity in BTU/hr-ft-F
-    return k * 1.730735;   // Convert to W/m-K
-}
-
-double Argon::k(double T) const {
-    const double a = 1.31e-3; // in BTU/hr-ft-F
-    const double b = 0.701;
-    double T_R = T * 9.0 / 5.0; // Convert K to °R
-    double k = a * std::pow(T_R, b); // Conductivity in BTU/hr-ft-F
-    return k * 1.730735;   // Convert to W/m-K
-}
-
-double Krypton::k(double T) const {
-    const double a = 1.588e-5; // in BTU/hr-ft-F
-    const double b = 0.92331;
-    double T_R = T * 9.0 / 5.0; // Convert K to °R
-    double k = a * std::pow(T_R, b); // Conductivity in BTU/hr-ft-F
-    return k * 1.730735;   // Convert to W/m-K
-}
-
-double Hydrogen::k(double T) const {
-    const double a = 5.834e-4; // in BTU/hr-ft-F
-    const double b = 0.8213;
-    double T_R = T * 9.0 / 5.0; // Convert K to °R
-    double k = a * std::pow(T_R, b); // Conductivity in BTU/hr-ft-F
-    return k * 1.730735;   // Convert to W/m-K
-}
-
-double Nitrogen::k(double T) const {
-    const double a = 7.35e-5; // in BTU/hr-ft-F
-    const double b = 0.846;
-    double T_R = T * 9.0 / 5.0; // Convert K to °R
-    double k = a * std::pow(T_R, b); // Conductivity in BTU/hr-ft-F
-    return k * 1.730735;   // Convert to W/m-K
-}
-
-double UO2::k(double T, double Bu, double gad) const {
-    if (T < 300.0 || T > 3000.0) {
-        throw std::out_of_range("Temperature must be within 300.0 and 3000.0 K for UO2 thermal conductivity");
-    }
-    if (Bu < 0.0 || Bu > 62.0) {
-        throw std::out_of_range("Burnup must be between 0.0 and 62.0 MWd/kgU for UO2 thermal conductivity");
-    }
-    if (gad < 0.0 || gad > 0.1) {
-        throw std::out_of_range("Gadolinium content must be between 0.0 and 0.10 wt. percent for UO2 thermal conductivity");
-    }
-
-    double h = 1 / (1.0 + 396.0 * std::exp(-6380.0 / T));
-    double k_phonon = 1 / (
-        0.0452 +
-        0.000246 * T +
-        0.00187 * Bu +
-        1.1599 * gad +
-        (1.0 - 0.9 * std::exp(-0.04 * Bu)) * 0.038 * h * std::pow(Bu, 0.28)
-    );
-    double k_electronic = 3.50e9 / (T * T) * std::exp(-16361.0 / T);
-
-    return k_phonon + k_electronic;
-}
-
-double UO2::Cp(double T, double Bu, double gad) const {
-    double OM = 2.0; // oxygen-to-metal ratio for UO2
-    double R = 8.314; // universal gas constant [J/mol-K]
-    double theta = 535.285; // Einstein temperature [K]
-    double E_D = 1.577e5; // Debye energy [J/mol]
-    double K1 = 296.7, K2 = 2.43e-2, K3 = 8.745e7; // empirical constants for UO2 heat capacity [J/kg-K]
-
-    return (
-        K1 * theta * theta * std::exp(theta / T) / (T * T * std::pow(std::exp(theta / T) - 1.0, 2.0)) +
-        K2 * T +
-        0.5 * OM * K3 * E_D / (R * T * T) * std::exp(-E_D / (R * T))
-    );
-}
-
-void UO2::update_node_radii(const std::shared_ptr<CylinderNode>& node, double T, double T_prev) const {
-    if (T < 0.0 || T_prev < 0.0) {
-        throw std::out_of_range("Temperature in Kelvin cannot be less than 0.0");
-    }
-
-    double strain = 1e-5 * (T - T_prev);
-    node->set_inner_radius(node->get_inner_radius() * (1.0 + strain));
-    node->set_outer_radius(node->get_outer_radius() * (1.0 + strain));
-}
 
 double Zircaloy::k(double T) const {
     if (T < 0.0) {
@@ -166,4 +78,100 @@ void Zircaloy::update_node_radii(const std::shared_ptr<CylinderNode>& node, doub
     // Update the node radii based on the calculated strain
     node->set_inner_radius(node->get_initial_inner_radius() * (1.0 + total_strain));
     node->set_outer_radius(node->get_initial_outer_radius() * (1.0 + total_strain));
+}
+
+double UO2::k(double T, double Bu, double gad) const {
+    if (T < 300.0 || T > 3000.0) {
+        throw std::out_of_range("Temperature must be within 300.0 and 3000.0 K for UO2 thermal conductivity");
+    }
+    if (Bu < 0.0 || Bu > 62.0) {
+        throw std::out_of_range("Burnup must be between 0.0 and 62.0 MWd/kgU for UO2 thermal conductivity");
+    }
+    if (gad < 0.0 || gad > 0.1) {
+        throw std::out_of_range("Gadolinium content must be between 0.0 and 0.10 wt. percent for UO2 thermal conductivity");
+    }
+
+    double h = 1 / (1.0 + 396.0 * std::exp(-6380.0 / T));
+    double k_phonon = 1 / (
+        0.0452 +
+        0.000246 * T +
+        0.00187 * Bu +
+        1.1599 * gad +
+        (1.0 - 0.9 * std::exp(-0.04 * Bu)) * 0.038 * h * std::pow(Bu, 0.28)
+    );
+    double k_electronic = 3.50e9 / (T * T) * std::exp(-16361.0 / T);
+
+    return k_phonon + k_electronic;
+}
+
+double UO2::Cp(double T, double Bu, double gad) const {
+    double OM = 2.0; // oxygen-to-metal ratio for UO2
+    double R = 8.314; // universal gas constant [J/mol-K]
+    double theta = 535.285; // Einstein temperature [K]
+    double E_D = 1.577e5; // Debye energy [J/mol]
+    double K1 = 296.7, K2 = 2.43e-2, K3 = 8.745e7; // empirical constants for UO2 heat capacity [J/kg-K]
+
+    return (
+        K1 * theta * theta * std::exp(theta / T) / (T * T * std::pow(std::exp(theta / T) - 1.0, 2.0)) +
+        K2 * T +
+        0.5 * OM * K3 * E_D / (R * T * T) * std::exp(-E_D / (R * T))
+    );
+}
+
+void UO2::update_node_radii(const std::shared_ptr<CylinderNode>& node, double T, double T_prev) const {
+    if (T < 0.0 || T_prev < 0.0) {
+        throw std::out_of_range("Temperature in Kelvin cannot be less than 0.0");
+    }
+
+    double strain = 1e-5 * (T - T_prev);
+    node->set_inner_radius(node->get_inner_radius() * (1.0 + strain));
+    node->set_outer_radius(node->get_outer_radius() * (1.0 + strain));
+}
+
+double Helium::k(double T) const {
+    const double a = 1.314e-3; // in BTU/hr-ft-F
+    const double b = 0.668;
+    double T_R = T * 9.0 / 5.0; // Convert K to °R
+    double k = a * std::pow(T_R, b); // Conductivity in BTU/hr-ft-F
+    return k * 1.730735;   // Convert to W/m-K
+}
+
+double Xenon::k(double T) const {
+    const double a = 1.395e-5; // in BTU/hr-ft-F
+    const double b = 0.872;
+    double T_R = T * 9.0 / 5.0; // Convert K to °R
+    double k = a * std::pow(T_R, b); // Conductivity in BTU/hr-ft-F
+    return k * 1.730735;   // Convert to W/m-K
+}
+
+double Argon::k(double T) const {
+    const double a = 1.31e-3; // in BTU/hr-ft-F
+    const double b = 0.701;
+    double T_R = T * 9.0 / 5.0; // Convert K to °R
+    double k = a * std::pow(T_R, b); // Conductivity in BTU/hr-ft-F
+    return k * 1.730735;   // Convert to W/m-K
+}
+
+double Krypton::k(double T) const {
+    const double a = 1.588e-5; // in BTU/hr-ft-F
+    const double b = 0.92331;
+    double T_R = T * 9.0 / 5.0; // Convert K to °R
+    double k = a * std::pow(T_R, b); // Conductivity in BTU/hr-ft-F
+    return k * 1.730735;   // Convert to W/m-K
+}
+
+double Hydrogen::k(double T) const {
+    const double a = 5.834e-4; // in BTU/hr-ft-F
+    const double b = 0.8213;
+    double T_R = T * 9.0 / 5.0; // Convert K to °R
+    double k = a * std::pow(T_R, b); // Conductivity in BTU/hr-ft-F
+    return k * 1.730735;   // Convert to W/m-K
+}
+
+double Nitrogen::k(double T) const {
+    const double a = 7.35e-5; // in BTU/hr-ft-F
+    const double b = 0.846;
+    double T_R = T * 9.0 / 5.0; // Convert K to °R
+    double k = a * std::pow(T_R, b); // Conductivity in BTU/hr-ft-F
+    return k * 1.730735;   // Convert to W/m-K
 }
