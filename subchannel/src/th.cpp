a729c6e38b403b592746f247f5b48227cf8f8f7d
MicahBest <micahbest21@gmail.com>
2025-09-04 12:30:58 -0400

    Addressed Copilot review comments

--- subchannel/src/th.cpp
+++ subchannel/src/th.cpp
@@ -2,8 +2,6 @@
 
 void TH::solve_enthalpy(std::vector<double>& h, const std::vector<double>& W_l, double lhr, const Geometry& geom) {
 
-    std::cout << "Solving enthalpy in TH module..." << std::endl;
-
     // Perform calculations for each axial plane
     for (size_t k = 1; k < geom.naxial() + 1; ++k) {
         h[k] = (W_l[k] * h[k-1] + geom.dz() * lhr) / W_l[k];
@@ -18,8 +16,6 @@ void TH::solve_pressure(
     const Geometry& geom
 ) {
 
-    std::cout << "Solving pressure in TH module..." << std::endl;
-
     double G; // mass flux
     double Re; // Reynolds number
     double f; // single phase friction factor
@@ -27,12 +23,16 @@ void TH::solve_pressure(
     double dP_fric; // single phase frictional pressure drop
     double dP_grav; // single phase gravitational pressure drop
 
+    // coefficients for Adams correlation from ANTS Theory
+    const double a_1 = 0.1892;
+    const double n = -0.2;
+
     for (size_t k = 1; k < geom.naxial() + 1; ++k) {
         G = W_l[k] / geom.flow_area();
         Re = W_l[k] * geom.hydraulic_diameter() / (geom.flow_area() * mu[k]);
 
         // calculate frictional pressure drop
-        f = 0.1892 * pow(Re, -0.2);
+        f = a_1 * pow(Re, n);
         K = f * geom.dz() / geom.hydraulic_diameter();
         dP_fric = K * G * G / (2.0 * rho[k]);
 
