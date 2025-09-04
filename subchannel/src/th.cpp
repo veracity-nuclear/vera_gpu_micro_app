81cbcc7b5e5ec10ca791e2d34ba81dbc78c3ee4b
MicahBest <micahbest21@gmail.com>
2025-09-04 11:50:54 -0400

    Calculate single phase pressure drop

--- subchannel/src/th.cpp
+++ subchannel/src/th.cpp
@@ -9,3 +9,36 @@ void TH::solve_enthalpy(std::vector<double>& h, const std::vector<double>& W_l, 
         h[k] = (W_l[k] * h[k-1] + geom.dz() * lhr) / W_l[k];
     }
 }
+
+void TH::solve_pressure(
+    std::vector<double>& P,
+    const std::vector<double>& W_l,
+    const std::vector<double>& rho,
+    const std::vector<double>& mu,
+    const Geometry& geom
+) {
+
+    std::cout << "Solving pressure in TH module..." << std::endl;
+
+    double G; // mass flux
+    double Re; // Reynolds number
+    double f; // single phase friction factor
+    double K; // frictional loss coefficient
+    double dP_fric; // single phase frictional pressure drop
+    double dP_grav; // single phase gravitational pressure drop
+
+    for (size_t k = 1; k < geom.naxial() + 1; ++k) {
+        G = W_l[k] / geom.flow_area();
+        Re = W_l[k] * geom.hydraulic_diameter() / (geom.flow_area() * mu[k]);
+
+        // calculate frictional pressure drop
+        f = 0.1892 * pow(Re, -0.2);
+        K = f * geom.dz() / geom.hydraulic_diameter();
+        dP_fric = K * G * G / (2.0 * rho[k]);
+
+        // calculate gravitational pressure drop
+        dP_grav = rho[k] * 9.81 * geom.dz();
+
+        P[k] = P[k-1] - dP_fric - dP_grav;
+    }
+}
--- subchannel/src/th.hpp
+++ subchannel/src/th.hpp
@@ -3,11 +3,20 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <cmath>
 
 #include "geometry.hpp"
+#include "materials.hpp"
 
 namespace TH {
 
 void solve_enthalpy(std::vector<double>& h, const std::vector<double>& W_l, double lhr, const Geometry& geom);
+void solve_pressure(
+    std::vector<double>& P,
+    const std::vector<double>& W_l,
+    const std::vector<double>& rho,
+    const std::vector<double>& mu,
+    const Geometry& geom
+);
 
 } // namespace TH
