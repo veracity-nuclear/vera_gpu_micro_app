55254e733fc26458d52d6216d31681b0af36910c
MicahBest <micahbest21@gmail.com>
2025-09-08 11:10:28 -0400

    Mixture mass flow calculation

--- subchannel/src/state.hpp
+++ subchannel/src/state.hpp
@@ -8,4 +8,12 @@ struct State {
     Vector1D W_l;    // liquid mass flow rate
     Vector1D W_v;    // vapor mass flow rate
     Vector1D alpha;  // void fraction
+
+    // mixture mass flow rate
+    Vector1D W_m() const {
+        return W_l + W_v;
+    }
+    double W_m(size_t i) const {
+        return W_l[i] + W_v[i];
+    }
 };
